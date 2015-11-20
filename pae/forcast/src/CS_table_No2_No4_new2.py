68b383c29123147c3d1e8c8772a8ef770a3915ed
wasit7 <wasit7@gmail.com>
2015-11-20 13:10:35 +0700

    add registration table into db

--- pae/forcast/src/CS_table_No2_No4_new2.py
+++ pae/forcast/src/CS_table_No2_No4_new2.py
@@ -13,9 +13,15 @@ df_file = pd.read_csv('../data/CS_table_No2_No4_new.csv',delimiter=";", skip_bla
                  error_bad_lines=False,encoding='utf8')
                  
 
-
-
+df=pd.DataFrame({'STUDENTID':df_file.STUDENTID,
+                 'ACADYEAR':df_file.ACADYEAR,
+                 'SEMESTER':df_file.SEMESTER,
+                 'COURSEID':df_file.COURSEID,
+                 'GRADE':df_file.GRADE
+                 })
+column_order=['STUDENTID','ACADYEAR','SEMESTER','COURSEID','GRADE']
+df = df[column_order] 
 con = sql.connect("project.sqlite")
 #df = pd.DataFrame({'TestData': [1, 2, 3, 4, 5, 6, 7, 8, 9]}, dtype='float')
-pd_sql.to_sql(df_file, "registration", con)
+pd_sql.to_sql(df, "registration", con)
 con.close()
