0138b50fcb392105a38e537a4d39a07d5ba39d02
wasit7 <wasit7@gmail.com>
2015-11-20 12:28:35 +0700

    add new example

--- pae/forcast/src/CS_table_No2_No4_new2.py
+++ pae/forcast/src/CS_table_No2_No4_new2.py
@@ -17,5 +17,5 @@ df_file = pd.read_csv('../data/CS_table_No2_No4_new.csv',delimiter=";", skip_bla
 
 con = sql.connect("project.sqlite")
 #df = pd.DataFrame({'TestData': [1, 2, 3, 4, 5, 6, 7, 8, 9]}, dtype='float')
-pd_sql.write_frame(df_file, "registration", con)
+pd_sql.to_sql(df_file, "registration", con)
 con.close()
