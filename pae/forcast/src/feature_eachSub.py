5f47b1f44d5baf4ac4a763c85da4bc70316ebcfb
paemethinee <m-methinee@hotmail.com>
2016-05-14 23:55:03 +0700

    edit book's db, edit training data by droping subject that less than 20 and make a classification file in jupyter

--- pae/forcast/src/feature_eachSub.py
+++ pae/forcast/src/feature_eachSub.py
@@ -13,15 +13,18 @@ from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import cross_val_score
 book = xlwt.Workbook(encoding="utf-8")
-sheet1 = book.add_sheet("Precission Without Merge")
-sheet2 = book.add_sheet("Precission With Merge")
+sheet1 = book.add_sheet("Precission Only Subject")
+
 
 
 #----------------------Traning Data With Merging-----------------------
 #df_file = pd.read_csv('../data/df_more20.csv',delimiter=",", skip_blank_lines = True, 
 #                 error_bad_lines=False)
-df_file = pd.read_csv('../data/df_sub_more20_merge.csv',delimiter=",", skip_blank_lines = True, 
+df_file = pd.read_csv('../data/df_dropSub_less20.csv',delimiter=",", skip_blank_lines = True, 
                  error_bad_lines=False)
+df_file = df_file.fillna(0)
+df_file = df_file.replace(['A', 'B+', 'B', 'C+', 'C' , 'D+' , 'D' , 'F' , 'W' , 'S' , 'S#' , 'U' , 'U#'], 
+                     [8, 7, 7, 6 , 6, 5, 5, 4, 3, 2, 2, 1, 1])
                  
 count_courseId = df_file["3COURSEID"].value_counts() 
 more20 = count_courseId
@@ -30,7 +33,7 @@ headers=list(df_file.columns.values)
 subjects = []
 countSub = 0
 #Create dictionary of list subjects
-for sub in df_file[headers[4]]:
+for sub in df_file[headers[1]]:
     if sub not in subjects:
         subjects.append(sub)
         countSub = countSub+1
@@ -40,7 +43,7 @@ subjects.sort()
 precision_rf={}
 df_precision = more20.copy()
 
-list_allsub = df_file.columns[6:]
+list_allsub = df_file.columns[4:]
 allSubject_df = pd.DataFrame(columns=[subjects],index=[list_allsub])
 top10_df = pd.DataFrame(columns=[subjects])
 
@@ -54,9 +57,9 @@ for subject in subjects:
     #print "Number of %s enrollment: %s"%(subject,count_enrollment)
 
     A = df_sub.as_matrix()
-    X = A[:,6:]
+    X = A[:,6:117]
     X = X.astype(np.int64, copy=False)
-    y = A[:,5]
+    y = A[:,2]
     y = y.astype(np.int64, copy=False)
 
     #Training data
@@ -69,8 +72,8 @@ for subject in subjects:
     precision_rf[subject] = scores.mean()
     df_precision.loc[subject]=precision_rf[subject]
     
-    sheet2.write(count, 0, subject)
-    sheet2.write(count,1, scores.mean())
+    sheet1.write(count, 0, subject)
+    sheet1.write(count,1, scores.mean())
     book.save("RF_crossvalidation.xls")
     count = count+1
     
@@ -85,7 +88,7 @@ for subject in subjects:
     std = np.std([tree.feature_importances_ for tree in forest.estimators_],
              axis=0)
     indices = np.argsort(importances)[::-1]
-    list_grade = df_file.columns[6:]
+    list_grade = df_file.columns[4:]
     # Print the feature ranking
     print("Feature ranking:")
 
@@ -99,6 +102,7 @@ for subject in subjects:
         top10_df.loc[i,subject] = str(top10[i-1])
     print "-----------------------------------"
 
+df_precision.plot(kind='bar')
 
 writer = pd.ExcelWriter("feature_eachSub.xlsx")
 pd.DataFrame(allSubject_df).to_excel(writer,"all_feature")
